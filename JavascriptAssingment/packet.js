5f40893c93e6ea36f307bb52e7090b2cc19c292a
Josue Rodriguez <jrod_1729@hotmail.com>
2018-08-27 15:16:08 -0400

    Completed JS

--- JavascriptAssingment/packet.js
+++ JavascriptAssingment/packet.js
@@ -30,9 +30,9 @@ function bubbleSort(numArray) {
     for(let j=0;j<numArray.length;j++) {
         for(let i = 0; i < numArray.length; i++) {
             if(numArray[i]>numArray[i+1]) {
-                let temp = numArray[i];
+                let arr = numArray[i];
                 numArray[i] = numArray[i+1];
-                numArray[i+1] = temp;
+                numArray[i+1] = arr;
             }
         }
     }      
@@ -154,12 +154,26 @@ function printShape(shape, height, character){
 // Define function traverseObject(someObj)
 // Print every property and it's value.
 
+function traverseObject(someObj){
+
+
+}
+
 // 10. Delete Element
 // Define function deleteElement(someArr)
 // Print length
 // Delete the third element in the array.
 // Print length
 // The lengths should be the same.
+
+function deleteElement(someArr){
+    console.log(someArr.length);
+    delete someArr[2];
+    console.log(someArr.length);
+}
+
+console.log(deleteElement([1,2,3,4,5]));
+console.log('');
 
 // 11. Splice Element
 // Define function spliceElement(someArr)
@@ -168,6 +182,14 @@ function printShape(shape, height, character){
 // Print length
 // The lengths should be one less than the original length.
 
+function spliceElement(someArr){
+    console.log(someArr.length);
+    someArr.splice(2,1);
+    console.log(someArr.length);
+}
+
+console.log(spliceElement([1,2,3,4,5]));
+console.log('');
 // 12. Defining an object using a constructor
 // Define a function Person(name, age)
 // The following line should set a Person object to the variable john:
@@ -214,36 +236,77 @@ console.log('');
 // Find the html element that contains "USA".
 // Print that element's contents.
 
-// function getUSA(){
-//     let usa = document.get
-// }
+function getUSA(){
+    let usa = document.getElementsByTagName("span");
+    for (let i = 0; i < usa.length; i++){
+        if(usa[i].textContent === "USA"){
+            console.log(usa[i].innerHTML);
+        }
+    }
+}
+
+//console.log(getUSA());
 
 // 2. Sales
 // Define function getPeopleInSales()
 // Print the names of all the people in the sales department.
-// function getPeopleInSales(){
-//     let peopleInSales = document.getElementsByClassName("empName");
-//     let values = peopleInSales[0].value;
-    
-    
-// }
-// console.log(getPeopleInSales());
+
+function getPeopleInSales(){
+    let peopleInSales = document.getElementsByClassName("empName");
+
+    for(let i = 0; i < peopleInSales.length; i++){
+        let empDep = peopleInSales[i].parentElement.getElementsByTagName("td");
+        for(let x = 0; x < empDep.length; x++){
+            if(empDep[x].textContent === "Sales"){
+                 console.log(peopleInSales[i].innerHTML);
+            }
+        }
+    } 
+}
+//console.log(getPeopleInSales());
   
 // 3. Click Here
 // Define function getAnchorChildren()
 // Find all anchor elements with a <span> child.
 // Print the contents of <span>
+
+function getAnchorChildren(){
+    let a = document.getElementsByTagName("a");
+
+    for(let i = 0; i < a.length; i++){
+        let span = a[i].getElementsByTagName("span");
+        for(let x = 0; x < span.length; x++){ 
+             console.log(span[i].innerHTML); 
+        }
+    } 
+}
   
 // 4. Hobbies
 // Define function getHobbies()
 // Find all checked options in the 'skills' select element.
 // Print the value and the contents.
+
+function getHobbies(){
+   let skill= document.getElementsByName("skills").value
+   console.log(document.innerHTML = skill);
+}
   
 // 5. Custom Attribute
 // Define function getCustomAttribute()
 // Find all elements with "data-customAttr" attribute
 // Print the value of the attribute.
 // Print the element that has the attribute.
+
+function getCustomAttribute(){
+    let elem = document.getElementsByTagName("*");
+
+    for(let i=0; i<elem.length; i++){
+        if(tags[i].hasAttribute("data-customAttr")){
+            console.log(`Value: ${elem[i].getAttribute("data-customAttr")}`);
+            console.log(`Element: ${elem[i].innerHTML}`);
+        }
+    }
+}
 
 // 6. Sum Event
 // NOTE: Write unobtrusive Javascript
@@ -257,11 +320,37 @@ console.log('');
 // Put the sum in the <span> element.
 // If values cannot be added, put "Cannot add" in the <span> element
 
+document.addEventListener('change', function() {
+    document.getElementById('num1').onchange=changeEventHandler;
+})
+
+document.addEventListener('change', function() {
+    document.getElementById('num2').onchange=changeEventHandler;
+})
+
+function changeEventHandler(event) {
+    let num1Val = document.getElementById('num1').value;
+    let num2Val = document.getElementById('num2').value;
+    let sum = document.getElementById('sum');
+    if (!(+num1Val + +num2Val)) {
+        sum.innerHTML = "Cannot add";
+    } else {
+        sum.innerHTML = +num1Val + +num2Val;
+    }
+}
+
 // 7. Skills Event
 // NOTE: Write unobtrusive Javascript
 // When user selects a skill, create an alert with a message similar to:
 // 	"Are you sure CSS is one of your skills?"
 // NOTE: no alert should appear when user deselects a skill.
+
+let options = document.getElementsByName('skills');
+for (let i of options) {
+    i.addEventListener('change', function() {
+        alert(`Are you sure ${this.value} is one of your skills?`);
+    });
+}
 
 // 8. Favorite Color Event
 // NOTE: Write unobtrusive Javascript
@@ -277,19 +366,59 @@ console.log('');
 // 	Hide the name if shown.
 // 	Show the name if hidden.
 
+let names = document.getElementsByClassName('empName');
+for (let i of names) {
+    i.addEventListener('mouseover', function() {
+        if (this.style.opacity === '0') {
+            this.style.opacity  = 1;
+        } else {
+            this.style.opacity  = 0; 
+        }
+    });
+}
+
 // 10. Current Time
 // Regarding this element:
 // 	<h5 id="currentTime"></h5>
 // Show the current time in this element in this format: 9:05:23 AM
 // The time should be accurate to the second without having to reload the page.
 
+function currentTime(){
+
+    var d = new Date();
+    var n = d.toLocaleString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    document.getElementById("currentTime").innerHTML = n;
+
+    setTimeout(function () {
+        currentTime()
+    }, 500);
+}
+currentTime();
+
 // 11. Delay
 // Regarding this element:
 // 	<p id="helloWorld">Hello, World!</p>
 // Three seconds after a user clicks on this element, change the text to a random color.
+
+function changeColor(){
+   let color = document.getElementById("helloWorld");
+    setTimeout(()=>{color.style.color = randomColors()},3000);
+}
+
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
 // 12. Walk the DOM
 // Define function walkTheDOM(node, func)
 // This function should traverse every node in the DOM. Use recursion.
 // On each node, call func(node).
 
+function walkTheDOM(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+    }
+}
